62da6fcf6a19f64ec27807fc3ab8184754105b7b
winnie48562000 <winnie48562000@gmail.com>
2021-07-23 09:27:06 +0800

    Set BodyParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require related modules used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -8,8 +9,16 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true}))
+
 // localhost:3000 
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
